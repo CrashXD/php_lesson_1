54c5fbbd2bf060b789723a0740f01d85f7d51dc4
Crashxd <crashxd@inbox.ru>
2023-03-12 13:06:47 +0600

    init

---
+++ script.php
@@ -0,0 +1,112 @@
+<pre>
+<?php
+//   print_r($_GET);
+//   echo "Hello, " . $_GET['firstname'];
+//   echo "Hello, {$_GET['firstname']}";
+
+// print_r($_GET);
+
+// if ($_GET['username'] == "admin") {
+//     echo 'Access!';
+// }
+
+// if ($_GET['username'] != "admin") {
+//     echo 'Warning!';
+// }
+
+// if ($_GET['username'] == "admin") {
+//     echo 'Access!';
+// } else {
+//     if ($_GET['username'] == 'moderator') {
+//         echo 'Success!';
+//     } else {
+//         if ($_GET['username'] == 'editor') {
+//             echo 'Edit!';
+//         } else {
+//             echo 'Warning!';
+//         }
+//     }
+// }
+
+// if ($_GET['username'] == "admin") {
+//     echo 'Access!';
+// } elseif ($_GET['username'] == 'moderator') {
+//     echo 'Success!';
+// } elseif ($_GET['username'] == 'editor') {
+//     echo 'Edit!';
+// } else {
+//     echo 'Warning!';
+// }
+
+// if (
+//     $_GET['username'] == "admin" ||
+//     $_GET['username'] == "moderator" ||
+//     $_GET['username'] == "editor"
+//     ) {
+//     echo 'Access!';
+// } else {
+//     echo 'Warning!';
+// }
+
+// switch ($_GET['username']) {
+//     case "admin":
+//         echo 'Access!';
+//         break;
+//     case "moderator":
+//         echo 'Success!';
+//         break;
+//     case "editor":
+//         echo 'Edit!';
+//         break;
+//     default:
+//         echo 'Warning!';
+//         break;
+// }
+
+// switch ($_GET['username']) {
+//     case "admin":
+//     case "moderator":
+//     case "editor":
+//         echo 'Access!';
+//         break;
+//     default:
+//         echo 'Warning!';
+//         break;
+// }
+
+// echo match ($_GET['username']) {
+//     "admin" => 'Access!',
+//     "moderator" => 'Success!',
+//     "editor" => 'Edit!',
+//     default => "Warning!",
+// };
+
+// echo match ($_GET['username']) {
+//     "admin", "moderator", "editor" => 'Access!',
+//     default => "Warning!",
+// };
+
+// $i = 0;
+
+// while ($i < 100) {
+//     echo $i . "\n";
+//     $i = $i + 5;
+// }
+
+
+// for ($i = 0; $i < 100; $i = $i + 10) {
+//     echo $i . "\n";
+// }
+
+$array = ["a", "b", "c", "d"];
+
+// foreach ($array as $key => $letter) {
+//     echo $key . ": " . $letter . "\n";
+// }
+
+// for ($i = 0; $i < count($array); $i++) {
+//     echo $i . ": " . $array[$i] . "\n";
+// }
+
+?>
+</pre>
